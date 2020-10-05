fb9b3eee58b31b3973b4f99583f77097817e6b9f
MaudeVanhengelPXL <maude.vanhengel@student.pxl.be>
2020-10-05 17:02:05 +0200

    PasswordUtil + Tests finished

--- src/main/java/be/pxl/ja/streamingservice/util/PasswordUtil.java
+++ src/main/java/be/pxl/ja/streamingservice/util/PasswordUtil.java
@@ -5,6 +5,44 @@ public class PasswordUtil {
 	private static final String SPECIAL_CHARACTERS = "~!@#$%^&*()_-";
 
 	public static int calculateStrength(String password) {
-		return 5;
+		int score;
+		if (password.length() < 6) {
+			score = 0;
+		} else {
+			if (password.length() < 10) {
+				score = 1;
+			} else {
+				score = 2;
+			}
+			boolean containsDigits = false;
+			boolean containsLowerCaseLetters = false;
+			boolean containsUpperCaseLetters = false;
+			boolean containsSpecialCharacters = false;
+			for (int i = 0; i < password.length(); i++) {
+				char character = password.charAt(i);
+				if (Character.isDigit(character)) {
+					containsDigits = true;
+				}
+				if (Character.isLowerCase(character)) {
+					containsLowerCaseLetters = true;
+				}
+				if (Character.isUpperCase(character)) {
+					containsUpperCaseLetters = true;
+				}
+				if (SPECIAL_CHARACTERS.indexOf(character) != -1) {
+					containsSpecialCharacters = true;
+				}
+			}
+			if (containsDigits) {
+				score += 2;
+			} if (containsLowerCaseLetters) {
+				score += 2;
+			} if (containsUpperCaseLetters) {
+				score += 2;
+			} if (containsSpecialCharacters) {
+				score += 2;
+			}
+		}
+		return score;
 	}
 }
--- src/test/java/be/pxl/ja/streamingservice/util/PasswordUtilCalculateStrengthTest.java
+++ src/test/java/be/pxl/ja/streamingservice/util/PasswordUtilCalculateStrengthTest.java
@@ -1,5 +1,51 @@
 package be.pxl.ja.streamingservice.util;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PasswordUtilCalculateStrengthTest {
+
+    @Test
+    public void PasswordWithLessThan6CharactersIsAlways0() {
+        String password = "aaaa";
+        assertEquals(0, PasswordUtil.calculateStrength(password));
+    }
+
+    @Test
+    public void PasswordBetween6And10CharactersOnlyLowerCaseLetters() {
+        String password = "abcdef";
+        assertEquals(1 + 2, PasswordUtil.calculateStrength(password));
+    }
+
+    @Test
+    public void PasswordBetween6And10CharactersOneUpperCase() {
+        String password = "abcDefg";
+        assertEquals(1 + 2 + 2, PasswordUtil.calculateStrength(password));
+    }
+
+    @Test
+    public void PasswordBetween6And10CharactersSpecialCharacter() {
+        String password = "abc%efg";
+        assertEquals(1 + 2 + 2, PasswordUtil.calculateStrength(password));
+    }
+
+    @Test
+    public void PasswordLowerCaseUpperCaseDigitsAndSpecialCharacters() {
+        String password = "aBcd3f$g";
+        assertEquals(1 + 2 + 2 + 2 + 2, PasswordUtil.calculateStrength(password));
+    }
+
+    @Test
+    public void PasswordExactlyTenCharacters() {
+        String password = "abcdefghij";
+        assertEquals(2 + 2, PasswordUtil.calculateStrength(password));
+    }
+    @Test
+    public void PasswordMoreThanTenLowerCaseUpperCaseDigitsAndSpecialCharacters() {
+        String password = "abcdefhjS12$$$";
+        assertEquals(2 + 2 + 2 + 2 + 2, PasswordUtil.calculateStrength(password));
+    }
 
 }
